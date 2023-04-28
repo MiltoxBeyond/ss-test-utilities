3a2777919e6a2c998431432d781c3d7886e3cc7c
MiltoxBeyond <mzurita85@gmail.com>
2023-04-28 07:20:34 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+var (
+  TOKEN_CHROME = map[string]string{
+    u: "",
+    k: "",
+    a: "",
+  }
+  TOKEN_FIREFOX = map[string]string{
+    u: "",
+    k: "",
+    a: "",
+  }
+  TOKEN_SAFARI = map[string]string{
+    u: "",
+    k: "",
+    a: "",
+  }
+  TOKEN_EDGE = map[string]string{
+    u: "",
+    k: "",
+    a: "",
+  }
+)
